e4d82ab60896f01b5db44b08310c48e2fd621ce1
Eyosias Woldemichael <eyosias16@gmail.com>
2024-06-10 20:19:39 +0200

    adjusted media query for phones

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,67 +4,76 @@ import profile from '../assets/profile1.jpg';
 
 
 function Project() {
-    return (
-        <div className='Project'>
-            <div>
-                <h2>Featured Projects</h2>
-                <ul>
-                    <li>
-                        <div className='proj-li'>
-                            <div className='cal'>
-                                <span className='bar'></span>
-                                <span className='date'> September 2, 2022</span>
-                            </div>
-                            <div className='project-icon'>
-                                <img src={profile} alt="profile Logo" className="profile-img" />
-                            </div>
-                            <h3>Planetaria</h3>
-                            <p>Creating technology to empower civilians
-                                to explore space on their own terms.</p>
-                            <div className='link-proj-desc'>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                                    <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-                                </svg>
-                                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='proj-li'>
-                            <div className='project-icon'>
-                                <img src={profile} alt="profile Logo" className="profile-img" />
-                            </div>
-                            <h3>Planetaria</h3>
-                            <p>Creating technology to empower civilians
-                                to explore space on their own terms.</p>
-                            <div className='link-proj-desc'>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                                    <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-                                </svg>
-                                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
-                            </div>
-                        </div>
-                    </li>
-                    <li>
-                        <div className='proj-li'>
-                            <div className='project-icon'>
-                                <img src={profile} alt="profile Logo" className="profile-img" />
-                            </div>
-                            <h3>Planetaria</h3>
-                            <p>Creating technology to empower civilians
-                                to explore space on their own terms.</p>
-                            <div className='link-proj-desc'>
-                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                                    <path d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-                                </svg>
-                                <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
-                            </div>
-                        </div>
-                    </li>
-                </ul>
+return (
+<div className='Project'>
+    <h2>Featured Projects</h2>
+    <ul>
+        <li>
+            <div className='proj-li'>
+                <div className='cal'>
+                    <span className='bar'></span>
+                    <span className='date'> September 2, 2022</span>
+                </div>
+                <div className='project-icon'>
+                    <img src={profile} alt="profile Logo" className="profile-img" />
+                </div>
+                <h3>Planetaria</h3>
+                <p>Creating technology to empower civilians
+                    to explore space on their own terms.</p>
+                <div className='link-proj-desc'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                        <path
+                            d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+                    </svg>
+                    <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
+                </div>
             </div>
-        </div>
-    );
+        </li>
+        <li>
+            <div className='proj-li'>
+                <div className='cal'>
+                    <span className='bar'></span>
+                    <span className='date'> September 2, 2022</span>
+                </div>
+                <div className='project-icon'>
+                    <img src={profile} alt="profile Logo" className="profile-img" />
+                </div>
+                <h3>Planetaria</h3>
+                <p>Creating technology to empower civilians
+                    to explore space on their own terms.</p>
+                <div className='link-proj-desc'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                        <path
+                            d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+                    </svg>
+                    <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
+                </div>
+            </div>
+        </li>
+        <li>
+            <div className='proj-li'>
+                <div className='cal'>
+                    <span className='bar'></span>
+                    <span className='date'> September 2, 2022</span>
+                </div>
+                <div className='project-icon'>
+                    <img src={profile} alt="profile Logo" className="profile-img" />
+                </div>
+                <h3>Planetaria</h3>
+                <p>Creating technology to empower civilians
+                    to explore space on their own terms.</p>
+                <div className='link-proj-desc'>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
+                        <path
+                            d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
+                    </svg>
+                    <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
+                </div>
+            </div>
+        </li>
+    </ul>
+</div>
+);
 }
 
 export default Project;
