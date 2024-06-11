abc48cff06cb0a200558d54d735fd2283832e05e
Eyosias Woldemichael <eyosias16@gmail.com>
2024-06-11 09:57:44 +0200

    removed link from import

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css'
 import Home from './pages/Home';
 import About from './pages/About';
