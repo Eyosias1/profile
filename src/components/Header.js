d2ee23d2813f7db82c79681aabff371788e1e847
Eyosias Woldemichael <eyosias16@gmail.com>
2024-07-17 16:07:16 +0200

    home page finished by adding last thing the home logo svg

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,12 @@ import React, { useState } from 'react';
 import NavBar from './Navbar';
 import '../styles/Header.css';
 import logo from '../assets/profile1.jpg';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
+// <img src={logo} alt="Profile Logo" className={`logo ${isHomePage ? 'large' : '' }`} />
 function Header() {
+const location = useLocation();
+const isHomePage = location.pathname === '/profile';
 const [isLightMode, setIsLightMode] = useState(false);
 
 const toggleLightMode = () => {
@@ -16,8 +19,14 @@ return (
 <div className='header-div'>
 
   <header className="header">
-    <div className="img-icon-container">
-      <Link to="/profile"><img src={logo} alt="profile Logo" className="logo" /></Link>
+    <div className={`img-icon-container ${isHomePage ? 'home-page-img' : '' }`}>
+      <Link to="/profile">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512" className="logo">
+        <path
+          d="M575.8 255.5c0 18-15 32.1-32 32.1l-32 0 .7 160.2c0 2.7-.2 5.4-.5 8.1l0 16.2c0 22.1-17.9 40-40 40l-16 0c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1L416 512l-24 0c-22.1 0-40-17.9-40-40l0-24 0-64c0-17.7-14.3-32-32-32l-64 0c-17.7 0-32 14.3-32 32l0 64 0 24c0 22.1-17.9 40-40 40l-24 0-31.9 0c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2l-16 0c-22.1 0-40-17.9-40-40l0-112c0-.9 0-1.9 .1-2.8l0-69.7-32 0c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+      </svg>
+      </Link>
+      <div></div>
     </div>
 
     <NavBar />
