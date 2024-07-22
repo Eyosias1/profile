31267b72570fbc146cc440996ddc7b3db697789d
Eyosias Woldemichael <eyosias16@gmail.com>
2024-07-22 16:32:23 +0200

    finished projects page adding the filtering system with javascript for each projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,100 +1,190 @@
-import React from 'react';
-import '../styles/Projects.css'
-import profile from '../assets/profile1.jpg';
+import React, { useState } from "react";
+import "../styles/Projects.css";
+import profile from "../assets/profile1.jpg";
+import SvgIcon from "../components/SvgIcon"; // Import the SVG component
+import cid from "../assets/cid.jpg";
+import travel from "../assets/Travelit.svg";
+import car from "../assets/Car 64.png";
 
 function Projects() {
-return (
-<div className='project-page'>
-  <h1>
-    Things I’ve made trying to put my dent in the universe.
-  </h1>
-  <p>
-    I’ve worked on tons of little projects over the years but these are the ones that I’m most proud of. Many of them
-    are open-source, so if you see something that piques your interest, check out the code and contribute if you have
-    ideas for how it can be improved.
-  </p>
-  <div className='all-Project'>
-    <ul className='projects-grid'>
-      <li>
-        <div className='proj-grid'>
-          <div className="proj-grid-el">
-            <div className='project-icon'>
-              <img src={profile} alt="profile Logo" className="profile-img" />
-            </div>
-            <h3>Planetaria</h3>
-            <p>Creating technology to empower civilians
-              to explore space on their own terms.</p>
-            <div className='link-proj-desc'>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                <path
-                  d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-              </svg>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
-            </div>
-          </div>
+  const projects = [
+    {
+      id: 1,
+      title: "Calcule tes indémnités",
+      description:
+        "Calcule tes indémnités is the web app to ensuire you are being payed all your indemnites as a french worker.",
+      imgSrc: cid,
+      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      keywords: [
+        "ALL CATEGORIES",
+        "full stack",
+        "react",
+        "mern stack",
+        "javascript",
+        "python",
+      ],
+    },
+    {
+      id: 2,
+      title: "Travel-It",
+      description:
+        "Travel-It is the authentique tourisme app designed for young travelers seeking thrilling experiences and memorable journeys.",
+      imgSrc: travel,
+      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      keywords: [
+        "ALL CATEGORIES",
+        "FULL STACK",
+        "Ruby on rails",
+        "ruby",
+        "postgree",
+      ],
+    },
+    {
+      id: 3,
+      title: "Car-Rental",
+      description:
+        "Car-Rental is the premier web app designed to make car rentals and reservations a breeze.",
+      imgSrc: car,
+      link: "https://www.youtube.com/watch?v=dQw4w9WgXcQ",
+      keywords: [
+        "all categories",
+        "full stack",
+        "ruby on rails",
+        "ruby",
+        "postgree",
+      ],
+    },
+  ];
+  const [selectedKeywords, setSelectedKeywords] = useState([]);
+
+  const handleKeywordClick = (keyword) => {
+    const normalizedKeyword = keyword.toLowerCase();
+
+    setSelectedKeywords((prevKeywords) => {
+      const normalizedPrevKeywords = prevKeywords.map((kw) => kw.toLowerCase());
+
+      return normalizedPrevKeywords.includes(normalizedKeyword)
+        ? prevKeywords.filter((kw) => kw.toLowerCase() !== normalizedKeyword)
+        : [...prevKeywords, keyword];
+    });
+  };
+
+  const keywords = [
+    "all categories",
+    "MERN stack",
+    "Ruby on Rails",
+    "react",
+    "full stack",
+    "front end",
+    "back end",
+    "unity",
+  ];
+
+  const keywordsLang = [
+    "c",
+    "java",
+    "python",
+    "javascript",
+    "ruby",
+    "c#",
+    "assembly",
+  ];
+
+  const filteredProjects = projects.filter((project) =>
+    selectedKeywords.every((kw) =>
+      project.keywords
+        .map((prkw) => prkw.toLowerCase())
+        .includes(kw.toLowerCase()),
+    ),
+  );
+  const countKeywordOccurrences = (projects) => {
+    const keywordCount = {};
+
+    projects.forEach((project) => {
+      project.keywords.forEach((keyword) => {
+        const normalizedKeyword = keyword.toLowerCase();
+        if (keywordCount[normalizedKeyword]) {
+          keywordCount[normalizedKeyword]++;
+        } else {
+          keywordCount[normalizedKeyword] = 1;
+        }
+      });
+    });
+
+    return keywordCount;
+  };
+
+  const keywordCount = countKeywordOccurrences(projects);
+
+  const handleItemClick = (url) => {
+    window.location.href = url;
+  };
+
+  return (
+    <div className="project-page">
+      <h1>Things I’ve made trying to put my dent in the universe.</h1>
+      <p>
+        I’ve worked on tons of little projects over the years but these are the
+        ones that I’m most proud of. Many of them are open-source, so if you see
+        something that piques your interest, check out the code and contribute
+        if you have ideas for how it can be improved.
+      </p>
+      <div className="proj-keyword">
+        <h1>Explore all {projects.length} projects</h1>
+        <h3>Software developement</h3>
+        <div className="keyword-buttons">
+          {keywords.map((keyword) => (
+            <button
+              key={keyword}
+              onClick={() => handleKeywordClick(keyword)}
+              className={selectedKeywords.includes(keyword) ? "active" : ""}
+            >
+              {keyword.toUpperCase()} (
+              {keywordCount[keyword.toLowerCase()] || 0})
+            </button>
+          ))}
         </div>
-      </li>
-      <li>
-        <div className='proj-grid'>
-          <div className="proj-grid-el">
-            <div className='project-icon'>
-              <img src={profile} alt="profile Logo" className="profile-img" />
-            </div>
-            <h3>Planetaria</h3>
-            <p>Creating technology to empower civilians
-              to explore space on their own terms.</p>
-            <div className='link-proj-desc'>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                <path
-                  d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-              </svg>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
-            </div>
-          </div>
+        <h3>Programming languages</h3>
+        <div className="keyword-buttons">
+          {keywordsLang.map((keyword) => (
+            <button
+              key={keyword}
+              onClick={() => handleKeywordClick(keyword)}
+              className={selectedKeywords.includes(keyword) ? "active" : ""}
+            >
+              {keyword.toUpperCase()} (
+              {keywordCount[keyword.toLowerCase()] || 0})
+            </button>
+          ))}
         </div>
-      </li>
-      <li>
-        <div className='proj-grid'>
-          <div className="proj-grid-el">
-            <div className='project-icon'>
-              <img src={profile} alt="profile Logo" className="profile-img" />
-            </div>
-            <h3>Planetaria</h3>
-            <p>Creating technology to empower civilians
-              to explore space on their own terms.</p>
-            <div className='link-proj-desc'>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                <path
-                  d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-              </svg>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
-            </div>
-          </div>
-        </div>
-      </li>
-      <li>
-        <div className='proj-grid'>
-          <div className="proj-grid-el">
-            <div className='project-icon'>
-              <img src={profile} alt="profile Logo" className="profile-img" />
-            </div>
-            <h3>Planetaria</h3>
-            <p>Creating technology to empower civilians
-              to explore space on their own terms.</p>
-            <div className='link-proj-desc'>
-              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512">
-                <path
-                  d="M579.8 267.7c56.5-56.5 56.5-148 0-204.5c-50-50-128.8-56.5-186.3-15.4l-1.6 1.1c-14.4 10.3-17.7 30.3-7.4 44.6s30.3 17.7 44.6 7.4l1.6-1.1c32.1-22.9 76-19.3 103.8 8.6c31.5 31.5 31.5 82.5 0 114L422.3 334.8c-31.5 31.5-82.5 31.5-114 0c-27.9-27.9-31.5-71.8-8.6-103.8l1.1-1.6c10.3-14.4 6.9-34.4-7.4-44.6s-34.4-6.9-44.6 7.4l-1.1 1.6C206.5 251.2 213 330 263 380c56.5 56.5 148 56.5 204.5 0L579.8 267.7zM60.2 244.3c-56.5 56.5-56.5 148 0 204.5c50 50 128.8 56.5 186.3 15.4l1.6-1.1c14.4-10.3 17.7-30.3 7.4-44.6s-30.3-17.7-44.6-7.4l-1.6 1.1c-32.1 22.9-76 19.3-103.8-8.6C74 372 74 321 105.5 289.5L217.7 177.2c31.5-31.5 82.5-31.5 114 0c27.9 27.9 31.5 71.8 8.6 103.9l-1.1 1.6c-10.3 14.4-6.9 34.4 7.4 44.6s34.4 6.9 44.6-7.4l1.1-1.6C433.5 260.8 427 182 377 132c-56.5-56.5-148-56.5-204.5 0L60.2 244.3z" />
-              </svg>
-              <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'> planeteria.tech</a>
-            </div>
-          </div>
-        </div>
-      </li>
-    </ul>
-  </div>
-</div>
-);
+      </div>
+      <div className="all-Project">
+        <ul className="projects-grid">
+          {filteredProjects.map((project) => (
+            <li key={project.id} onClick={() => handleItemClick(project.link)}>
+              <div className="proj-grid">
+                <div className="proj-grid-el">
+                  <div className="project-icon">
+                    <img
+                      src={project.imgSrc}
+                      alt={`${project.title} Logo`}
+                      className="profile-img"
+                    />
+                  </div>
+                  <h3>{project.title}</h3>
+                  <p>{project.description}</p>
+                  <div className="link-proj-desc">
+                    <SvgIcon iconName="link_svg" />
+                    <a href={project.link}>{project.title}</a>
+                  </div>
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
 
 export default Projects;
