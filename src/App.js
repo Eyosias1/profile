f8e102e60d24538783aafbfbf861c15e08570c13
Eyosias Woldemichael <eyosias16@gmail.com>
2024-07-29 22:25:52 +0200

    i love my vanille :) but added and changed the navigation structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,29 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import './App.css'
-import Home from './pages/Home';
-import About from './pages/About';
-import Articles from './pages/Articles';
-import Projects from './pages/Projects';
-import Header from './components/Header';
-import Footer from './components/Footer';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Articles from "./pages/Articles";
+import Projects from "./pages/Projects";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 function App() {
-return (
-<Router>
-  <div className="App section">
-    <Header />
-    <Routes>
-      <Route path="/profile" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/articles" element={<Articles />} />
-      <Route path="/projects" element={<Projects />} />
-    </Routes>
-    <Footer />
-  </div>
-</Router>
-);
+  return (
+    <Router>
+      <div className="App section">
+        <Header />
+        <Home />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/articles" element={<Articles />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
+  );
 }
 
 export default App;
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,30 +3,30 @@ import { Link } from "react-router-dom";
 import "../styles/Footer.css";
 
 function Footer() {
-  return (
-    <div className="footer-section">
-      <span className="lin-sep"></span>
-      <footer>
-        <div className="footer-div">
-          <ul className="footer-items">
-            <li>
-              <Link to="/profile"> Home</Link>
-            </li>
-            <li>
-              <Link to="/projects"> Projects</Link>
-            </li>
-            <li>
-              <Link to="/about"> About</Link>
-            </li>
-            <li>
-              <Link to="/articles"> Article</Link>
-            </li>
-          </ul>
-          <p> © 2024 Eyosias W. All rights reserved</p>
-        </div>
-      </footer>
+return (
+<div className="footer-section">
+  <span className="lin-sep"></span>
+  <footer>
+    <div className="footer-div">
+      <ul className="footer-items">
+        <li>
+          <Link to="/"> Home</Link>
+        </li>
+        <li>
+          <Link to="/projects"> Projects</Link>
+        </li>
+        <li>
+          <Link to="/about"> About</Link>
+        </li>
+        <li>
+          <Link to="/articles"> Article</Link>
+        </li>
+      </ul>
+      <p> © 2024 Eyosias W. All rights reserved</p>
     </div>
-  );
+  </footer>
+</div>
+);
 }
 
 export default Footer;
